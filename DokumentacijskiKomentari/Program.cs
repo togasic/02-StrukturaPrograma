5729aa60b4c7e41d67f6dfe84d5cb96bf3a10e31
Ante Mišković <ante.miskovic@vsite.hr>
2017-10-19 19:47:25 +0200

    *dodali smo dokumentacijske komentare za metodu DuljinaDvijeRijeci()

--- DokumentacijskiKomentari/Program.cs
+++ DokumentacijskiKomentari/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 
 namespace Vsite.CSharp.StrukturaPrograma
-{
+{/// <summary>
+/// Primjer za predavanja. 
+/// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -27,7 +29,15 @@ namespace Vsite.CSharp.StrukturaPrograma
             return broj * broj;
         }
 
-
+        /// <summary>
+        ///   Izračunava ukupnu duljinu dvije riječi.
+        /// </summary>
+        /// <param name="prva">
+        /// Prva riječ.</param>
+        /// <param name="druga">
+        /// Druga riječ.</param>
+        /// <returns>
+        /// Ukupna duljina</returns>
         static int DuljinaDvijeRiječi(string prva, string druga)
         {
             return (prva + druga).Length;
