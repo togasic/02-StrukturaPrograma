360e42993410b89837b1201cbbe4f4c08c9bb706
Domagoj Halusek <dohaluse@vsite.hr>
2017-10-19 20:00:41 +0200

    Promjena postavke imenika

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Csharp.StrukturaPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa1 mk1 = new MojaKlasa1();
+            MojaKlasa2cs mk2 = new MojaKlasa2cs();
+        }
+    }
+}
