04724f93919d9047961ce28c0a870f2554f1f974
lukakrilic96 <lukakrilic96@gmail.com>
2017-10-19 20:00:19 +0200

    Pormjena postavki imenika

---
+++ postavke_imenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.StrukturaPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa_1 mk1 = new MojaKlasa_1();
+            MojaKlasa_2 mk2 = new MojaKlasa_2();
+        }
+    }
+}
