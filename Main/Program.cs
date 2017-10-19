a82083fe063c3214373b93ee600f027adb1c9652
Tihomir Kuhar <tikuhar@vsite.hr>
2017-10-19 19:39:49 +0200

    Riješili smo se suvišne Main metode

--- Main/Program.cs
+++ Main/Program.cs
@@ -4,11 +4,12 @@ namespace Vsite.CSharp.StrukturaPrograma
 {
     class Program
     {
+        /*
         static void Main(string[] args)
         {
             Console.WriteLine("Krećem iz Program.Main(string[] args)");
         }
-
+        */
         static void Main()
         {
             Console.WriteLine("Krećem iz Program.Main()");
