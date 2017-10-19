c24232e31eb15abc2405b738628ca3001299a39b
Luka N Sever <lukasever.56@gmail.com>
2017-10-19 19:59:28 +0200

    New program, new classes

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.StrukturaPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa1 MK1 = new MojaKlasa1();
+            MojaKlasa2 MK2 = new MojaKlasa2();
+        }
+    }
+}
