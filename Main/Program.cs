81b55d408e2508b297f0e517e09da29602fcc824
Ivan <zivac.zg@hotmail.com>
2017-10-19 19:40:36 +0200

    Komentari

--- Main/Program.cs
+++ Main/Program.cs
@@ -4,10 +4,10 @@ namespace Vsite.CSharp.StrukturaPrograma
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Krećem iz Program.Main(string[] args)");
-        }
+       // static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Krećem iz Program.Main(string[] args)");
+        //}
 
         static void Main()
         {
