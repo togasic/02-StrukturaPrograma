91f3bec9f363fb3622fa23d75c63ce5d7180375e
Perica Jurić <pejuric@vsite.hr>
2017-10-19 20:07:45 +0200

    u varijablama koristili svakakve znakove

--- Identifikatori/Program.cs
+++ Identifikatori/Program.cs
@@ -7,8 +7,12 @@ namespace Vsite.CSharp.StrukturaPrograma
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) { 
+           // int string
+             int  get = 5;
+            string Main = "Perica";
+            const int Četri = 46;
+            string 你好 = "hi";
         }
     }
 }
