4de2f8b20a172e8f3a06e7f3dd508d10348de4c2
Tihomir Kuhar <tikuhar@vsite.hr>
2017-10-19 20:08:42 +0200

    Riješili smo identifikatore

--- Identifikatori/Program.cs
+++ Identifikatori/Program.cs
@@ -9,6 +9,13 @@ namespace Vsite.CSharp.StrukturaPrograma
     {
         static void Main(string[] args)
         {
+            //int string = 5;
+            int get = 2;
+            int main = 3;
+            const int četrdesetšest = 47;
+            const int \u0161trdesetšest = 46;
+            const int бројШест = 6;
+            const string 你好 = "Hello";
         }
     }
 }
