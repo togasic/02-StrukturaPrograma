558765775726f6f115bf1cf24ef661287b202c21
Perica Jurić <pejuric@vsite.hr>
2017-10-19 19:40:08 +0200

    rjesili smo se suvisne main metode

--- Main/Program.cs
+++ Main/Program.cs
@@ -4,10 +4,10 @@ namespace Vsite.CSharp.StrukturaPrograma
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Krećem iz Program.Main(string[] args)");
-        }
+        //static void main(string[] args)
+        //{
+        //    console.writeline("krećem iz program.main(string[] args)");
+        //}
 
         static void Main()
         {
