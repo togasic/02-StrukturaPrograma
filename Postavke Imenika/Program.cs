df0925cb0810b1df1f3b1027ae3bc360370e98c9
Perica Jurić <pejuric@vsite.hr>
2017-10-19 20:00:10 +0200

    Napravili Klase MojaClassa1 i 2 u scopu namespacea VCSP

---
+++ Postavke Imenika/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.StrukturaPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaClassa1 mk1 = new MojaClassa1();
+            MojaClassa2 mk2 = new MojaClassa2();
+        }
+    }
+}
