013e0b5d4425a695e9063246bbe89dc776d7cb07
Stjepan Cagalj <stjepan_12@hotmail.com>
2017-10-19 19:40:14 +0200

    RIjesili smo se suvisne main metode

--- Main/Program.cs
+++ Main/Program.cs
@@ -4,10 +4,10 @@ namespace Vsite.CSharp.StrukturaPrograma
 {
     class Program
     {
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             Console.WriteLine("Krećem iz Program.Main(string[] args)");
-        }
+        }*/
 
         static void Main()
         {
